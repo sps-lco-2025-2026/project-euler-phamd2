6f61648162983e3e278e6967aeff8902510014fe
phamd2 <phamd@stpaulsschool.org.uk>
2026-02-09 18:57:50 +0000

    Update Program.cs

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Problem 31
+using System.Numerics;
+
 void coinsum()
 {
     int target = 200;
-    int[] coins = {1,2,5,10,20,50,100,200}; 
+    int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
 
-    long[] ways = new long[target+1];
+    long[] ways = new long[target + 1];
     ways[0] = 1;
 
     foreach (int coin in coins)
@@ -17,11 +19,15 @@ void coinsum()
     Console.WriteLine(ways[target]);
 }
 
+coinsum();
+
 // Problem 50
 void consecutiveprimesum()
 {
-    int n = 1000000;
-    List<int> primes = new List<int>{};
+    int highest = 0;
+    int currentTotal = 0;
+    int n = 1000;
+    List<int> primes = new List<int> { };
     for (int i = 2; i <= n; i++)
     {
         bool isPrime = true;
@@ -40,10 +46,12 @@ void consecutiveprimesum()
             primes.Add(i);
         }
     }
-    for (int j = 0; j<= primes.Count; j++)
+    for (int j = 0; j < primes.Count; j++)
     {
-        if 
+        currentTotal += primes[j];
+        if (primes.Contains(currentTotal))
+            highest = currentTotal;
     }
+    Console.WriteLine(highest);
 }
 
-consecutiveprimesum();
